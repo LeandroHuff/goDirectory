c7886510b36a49b5cc32cef4ebd4d699c0ec7066
Leandro <71608128+LeandroHuff@users.noreply.github.com>
2026-08-09 07:27:50 -0300

    Update by leandro on 2026-08-09 at 07:27:50
    Signed-off-by: Leandro <71608128+LeandroHuff@users.noreply.github.com>

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 Author: [Leandro D. Huff](https://github.com/LeandroHuff)
 
-Version: **1**.**0**.**2** 
+Version: **1**.**1**.**0** 
 
 Function goDir() (go directory) extend 'cd' command line using 'pushd' and 'popd' commands over a stack path list.
 
@@ -24,32 +24,32 @@ Function 'goDir()' accept some command line parameters that control how it move 
 
 Usage: **goDir** [_options_]
 
-| Options          | Description                                                                                   |
-|:-----------------|:----------------------------------------------------------------------------------------------|
-| goDir            | With no parameters, list the stack content.                                                   |
-| -h|--help        | Show this usage information.                                                                  |
-| -c|-clr|--clear  | Clear stack, let current path in stack, do no change from current directory.                  |
-| -v|--version     | Show version number.                                                                          |
-| -                | Remove current path from stack, move to the next available in stack.                          |
-| - - -            | Remove count - paths from stack, move to the next available in stack.                         |
-| - N              | Remove N paths from the stack after the current one, stay in current directory, max 9.        |
-| -N               | Remove current and N-1 paths from the stack, move to the next path available in stack, max 9. |
-| /dir             | Push path to stack and move to it.                                                            |
-| /dir1 .. /dirN   | Push path list into stack, change to last one in the list (N).                                |
-| ../              | Move 1 level back, push it into stack.                                                        |
-| ../../           | Move 2 levels back, push last one into stack.                                                 |
-| ../N             | Move N levels back, push last one into stack, max 9.                                          |
+| Options           | Description                                                                                   |
+|:------------------|:----------------------------------------------------------------------------------------------|
+| goDir             | With no parameters, list the stack content.                                                   |
+| -V|--version      | Show version number.                                                                          |
+| -h|--help         | Show this usage information.                                                                  |
+| -c|-clr|--clear   | Clear stack, let current path in stack, do no change from current directory.                  |
+| -                 | Remove current path from stack, move to the next available on stack.                          |
+| -[ - - ... -]     | Remove count '-' paths from stack, move to the next available on stack.                         |
+| - N               | Remove N paths from the stack after the current one, stay in current directory, max 9.        |
+| -N                | Remove current and N-1 paths from the stack, move to the next path available on stack, max 9. |
+| /path             | Push path to stack and move to it.                                                            |
+| /path1 ... /pathN | Push path list on stack, change to last one in the list (N).                                |
+| ../               | Move 1 level back, push it on stack.                                                        |
+| ../../            | Move 2 levels back, push last one on stack.                                                 |
+| ../N              | Move N levels back, push last one on stack, max 9.                                          |
 
-`-` Mean the top item in the stack list.
-`N` Mean 1 or N (counter/number of) items in the list.
+`-` Mean the top-1 item on the stack list.
+`N` Mean 1 or N (counter/number of) items on stack list.
 
 ### Stack list
 
 All **goDir**() functionalities is based on _pushd_ and _popd_ Linux commands, for more details can be obtained by **pushd** _--help_ or **pophd** _--help_ command line and parameters.
 
-The main resource is the path list, it's an internal system stack that store the last path moved in or from.
+The main resource is the path list, it's an internal system stack that store the last path moved on or from.
 
-Over this path, the script can change current directory by getting or adding that in the stack list for next changes.
+Over this path, the script can change current directory by getting or adding that on the stack list for next changes.
 
 `[top]                                           [bottom]`  
 `[0]         [1]         [2]         [3]         [4]`  
--- goDirectory.sh
+++ goDirectory.sh
@@ -1,111 +1,171 @@
-################################################################################
-# @file         goDirectory.sh
-# @brief        Source variables and functions to extend 'cd' command line by pushd and popd commands.
-# @version:     1.0.3
-# @author:      Leandro D. Huff
-# @license:     CC BY 4.0 - https://creativecommons.org/licenses/by/4.0/
-# @details:     source goDirectory.sh
-################################################################################
+#!/usr/bin/env bash
 
-# Must be sourced not running
-[[ "${BASH_SOURCE[0]}" == "${0}" ]] && { "\033[91merror\033[0m: $(basename $0) must be sourced not running." ; exit 1 ; }
+## @file        goDirectory.sh
+## @brief       Source variables and functions to extend 'cd' command line by pushd and popd commands.
+## @version:    1.0.7
+## @author:     Leandro D. Huff
+## @license:    CC BY 4.0 - https://creativecommons.org/licenses/by/4.0/
+## @details:    source goDirectory.sh
 
-# version number
-declare -a gdVersion=(1 0 3)
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]
+then
+    echo -e "\033[1;91m  error\033[0m: $(basename $0) must be sourced not running."
+    return 1
+fi
 
-# function to show a help and usage information.
-function usageGoDir()
+if [ "x$logFile" = 'x' ]
+then
+    echo -e "\033[1;91m  error\033[0m: logFile dependence must be loaded."
+    return 2
+fi
+
+## @brief   Check a string presence by regex parameter.
+function reMatch() { echo -n "$1" | grep --color=never -qaoP "$2" 2> /dev/null ; }
+
+## @brief   Check a string presence by regex parameter and return only matching string.
+function reGetStringMatch() { echo -n "$1" | grep --color=never -aoP "$2" 2> /dev/null ; }
+
+## @brief   Pop dir from top of stack and change to that dir.
+function popDirChange()  { popd &> /dev/null ; }
+
+## @brief   Push dir to top of stack and change to that dir.
+function pushDirChange() { pushd "$1" &> /dev/null ; }
+
+## @brief   Push dir if it is not the current directory.
+function pushDirNotCurrent() { if [ "$1" != "$PWD" ] ; then pushDirChange "$1" ; fi ; }
+
+## @brief   Pop dir from stack top-1 and let current dir on top.
+function popDirOnly()  { popd -n &> /dev/null ; }
+
+## @brief   Push dir to stack top-1 and let current dir on top.
+function pushDirOnly() { pushd -n "$1" &> /dev/null ; }
+
+## @brief   Remove last dash from string and return reminings.
+function removeLastDash() { echo -n ${1%-*} ; }
+
+## @brief   Change to dir.
+function changeDir() { if [ "$1" != "$PWD" ] ; then cd "$1" 2> /dev/null ; fi ; }
+
+## @brief   Push dir if it is not on top of stack and do not change to dir.
+function pushDirNotOnTopOnly()
 {
-    printf "\
-Function 'goDir()' (go dir) extend 'cd' command line using 'pushd' and 'popd' commands.
-Version: v${gdVersion[0]}.${gdVersion[1]}.${gdVersion[2]}
-
-Usage:
-  goDir|godir|gd  [options]
-
-Where:
-  goDir                 Main function's name.
-  [godir|gd]            Aliases for goDir() function.
-
-[options]:
-  empty                 Show the stack content list.
-  -h|--help             Show this usage information.
-  -c|-clr|--clear       Clear stack, let current path in stack, do no move from current directory.
-  -v|--version          Show application version.
-  - |--back             Remove current path from the stack, move to the next available in stack.
-  - - - -[.. -]         Remove count '-' paths from the stack, move to the next available in stack.
-  - N                   Remove N paths from the stack after the current one, stay in current directory.
-  -N                    Remove current and N-1 paths from the stack, move to the next path available in stack.
-  /path                 Push path to the stack and move to it.
-  /path/1 .. /path/N    Push N path(s) to the stack, move to last one in the list (N).
-  ..[/]                 Move 1 level back, push it into the stack.
-  ../..[/]              Move 2 levels back, push last one into the stack.
-  ..[/]N                Move N levels back, push last one into the stack.
-"
+    local regex="^$1 "
+    local list="$(dirs)"
+    if ! reMatch "$list" "$regex" ; then pushDirOnly "$1" ; fi
 }
 
-# function to change to/from directories by a stack using pushd and popd
-# commands.
-# goDir() function accept some arguments, look at usageGoDir() or pass -h as an argument.
+## @brief   Push dir if it is not on top of stack and change to dir.
+function pushDirNotOnTop()
+{
+    local regex="^$1 "
+    local list="$(dirs)"
+    if ! reMatch "$list" "$regex" ; then pushDirChange "$1" ; fi
+}
+
+## @brief   Go to directory and back by a stack of dirs.
 function goDir()
 {
+    local version='1.1.0'
+    local path=''
+    local -i len=0
     if [ $# -gt 0 ]
     then
         while [ -n "$1" ]
         do
             case "$1" in
-            -h|--help)
-                usageGoDir
+            -V|--version)
+                version "$version"
                 break
                 ;;
-            -v|--version)
-                echo "v${gdVersion[0]}.${gdVersion[1]}.${gdVersion[2]}"
+            -h|--help)
+                echo -e "\
+Function 'goDir()' (go dir) extend 'cd' command line using 'pushd' and 'popd' commands.
+Version: ${version}
+Usage: goDir  [options]
+[options]:
+  empty                 Show the stack content list.
+  -V|--version          Show application version.
+  -h|--help             Show this usage information.
+  -c|-clr|--clear       Clear stack, let current path on stack, do not move from current directory.
+  - |--back             Remove current path from the stack, move to the next available on stack.
+  -[ - - ... -]         Remove count '-' paths from the stack, move to the next available on stack.
+  - N                   Remove N paths from the stack after the current one, stay at current directory.
+  -N                    Remove current and N-1 paths from the stack, move to the next path available on stack.
+  /path                 Push path to the stack and move to it.
+  /path/1 .. /path/N    Push N path(s) to the stack, move to the last one for list (N).
+  ..[/]                 Move 1 level back, push it on the stack.
+  ../..[/]              Move 2 levels back, push last one on the stack.
+  ..[/]N                Move N levels back, push last one on the stack.
+"
+                break
                 ;;
-            -c|-clr|--clear) 
-                while popd -n > /dev/null 2>&1
+            -c|-clr|--clear)
+                while popDirOnly
                 do
                     :
                 done
                 ;;
-            -)
-                if echo -n "${2}" | grep -aoP '^[0-9]$' > /dev/null 2>&1
+            -[0-9])
+                if (($1 < 0))
                 then
-                    shift
-                    declare -i items=$1
-                    while (( items > 1 ))
-                    do
-                        popd -n > /dev/null 2>&1 || return $?
-                        items=$((items-1))
-                    done
-                    if (( items == 1 ))
-                    then
-                        popd > /dev/null 2>&1 || return $?
-                    fi
-                else
-                    popd > /dev/null 2>&1 || return $?
+                    for ((i=$1 ; i<-1 ; i++)) ; do popDirOnly || return $? ; done
+                    popDirChange || return $?
                 fi
                 ;;
-            -[1-9])
-                for ((i=$1 ; i<0 ; i++))
-                do
-                    popd > /dev/null 2>&1 || return $?
-                done
+            -*)
+                local reNumericParam='^- *[1-9] *$'
+                local reDashParam='^(- *){2,}$'
+                local reNumberOnly='[1-9]'
+                if reMatch "$*" "$reNumericParam"
+                then
+                    if reMatch "$*" "$reNumberOnly"
+                    then
+                        len=$(reGetStringMatch "$*" "$reNumberOnly")
+                        while ((len > 1))
+                        do
+                            popDirOnly || return $?
+                            ((len--))
+                        done
+                    fi
+                elif reMatch "$*" "$reDashParam"
+                then
+                    path="$*"
+                    while reMatch "$path" "$reDashParam"
+                    do
+                        popDirOnly || return $?
+                        path="$(removeLastDash ${path})"
+                    done
+                fi
+                popDirChange || return $?
                 ;;
             *)
-                local path="$1"
-                declare -i len=0
-                if echo -n "${path}" | grep -aoP '^\.\.\/? *[0-9]$' > /dev/null 2>&1
+                local reNumericParam='^ *\.\./? *[1-9] *$'
+                local reDotsParam='^ *(\.\./?)|((\.\./)+) *$'
+                local reNumberOnly='[0-9]'
+                local stepBack='../'
+                local rootDir='/'
+                path="$1"
+                if reMatch "$path" "$reNumericParam"
                 then
-                    pushd -n "${PWD}" > /dev/null 2>&1 || return $?
-                    len=$(echo -n "${path}" | grep -aoP '[0-9]$')
-                    while (( len > 0 )) && ! [ "$PWD" = '/' ]
-                    do
-                        cd ../ > /dev/null 2>&1 || return $?
-                        ((len--))
-                    done
-                    pushd "${PWD}" > /dev/null 2>&1 || return $?
+                    pushDirNotOnTopOnly "$PWD"
+                    len=$(reGetStringMatch "$path" "$reNumberOnly")
+                    if ((len > 0))
+                    then
+                        while ((len > 0)) && ! [ "$PWD" = "$rootDir" ]
+                        do
+                            changeDir "$stepBack" || return $?
+                            ((len--))
+                        done
+                    fi
+                    pushDirNotOnTop "$PWD" || return $?
+                elif reMatch "$path" "$reDotsParam"
+                then
+                    if ! [ "$PWD" = "$rootDir" ]
+                    then
+                        pushDirNotOnTop "$path" || return $?
+                    fi
                 else
-                    pushd "${path}" > /dev/null 2>&1 || return $?
+                    pushDirNotCurrent "$path"
                 fi
                 ;;
             esac
@@ -114,7 +174,8 @@ function goDir()
     else
         dirs
     fi
+
+    return 0
 }
 
-# function to check if goDirectory was loaded.
-function gdLoaded() { echo -n "v${gdVersion[0]}.${gdVersion[1]}.${gdVersion[2]}" ; }
+declare goDirFile='loaded'
